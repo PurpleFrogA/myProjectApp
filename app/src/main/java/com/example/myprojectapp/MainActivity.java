18efe732ad510c62a4ecb1cf1d9d0f21285fe454
PurpleFrogA <asmaahussien410@gmail.com>
2023-06-20 22:23:34 +0300

    haha

--- app/src/main/java/com/example/myprojectapp/MainActivity.java
+++ app/src/main/java/com/example/myprojectapp/MainActivity.java
@@ -3,13 +3,27 @@ package com.example.myprojectapp;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() != null) {
+            // المستخدم مسجل الدخول بالفعل
+            // يمكنك توجيهه إلى الشاشة الرئيسية أو أي عملية أخرى هنا
+            Intent intent = new Intent(MainActivity.this, AnotherActivity.class);
+            startActivity(intent);
+            finish();
+            return; // يجب إنهاء النشاط هنا إذا كنت ترغب في تجاهل الشيفرة اللاحقة
+        }
+
         setContentView(R.layout.activity_main);
         gotoWelocme();
         //gotoLoginFragment();
